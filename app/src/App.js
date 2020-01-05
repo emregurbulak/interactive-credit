fad09578ba4c263ff56bca61781b7cbb15e7bfa1
emregurbulak <emre.gurbulak@gmail.com>
2020-01-05 23:07:00 +0300

    added input areas

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
+
+    this.state = {textFieldValue: ''};
+  }
+
+  handleTextFieldChange(e) {
+        this.setState({
+            textFieldValue: e.target.value
+        });
+  }
+
+  showIdentityErrMsg(){
+    return (this.state.textFieldValue.length > 0 && this.state.textFieldValue.length != 11) ? "Kimlik numarası 11 karakterden oluşmalıdır." :  "" 
+  }
+
+  render(){
+    return (
+      <div className="app-container">
+        <div className="dsc-title">
+            <p>Aşağıdaki bilgileri doldurarak kredi başvurunuzu tamamlayabilirsiniz</p>  
+        </div>
+        <div className="app-modal">
+          <div className="credit-inputs">
+            <TextField
+              id="outlined-number"
+              label="Kimlik Numarası"
+              type="number"
+              InputLabelProps={{shrink: true}}
+              value={this.state.textFieldValue} 
+              onChange={this.handleTextFieldChange}
+              variant="outlined"
+              helperText={this.showIdentityErrMsg()}
+              error={false}
+            />
+          </div>
+          <div className="credit-inputs">
+            <TextField 
+              id="outlined-basic" 
+              label="Adınız" 
+              variant="outlined" 
+            />
+          </div>
+          <div className="credit-inputs">
+            <TextField 
+              id="outlined-basic" 
+              label="Soyadınız" 
+              variant="outlined" 
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
